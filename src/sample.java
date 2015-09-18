71eaee673b61b5f3d62a2fbd3fb7d18219f3145f
oluw5470 <oluw5470@ISS147D8-11.scdsb.pri>
2015-09-18 10:18:42 -0400

    nreal commit

---
+++ src/sample.java
@@ -0,0 +1,28 @@
+import hsa.*;
+import BreezySwing.Format;
+public class sample {
+
+    public static void main(String[] args) {
+       double pop, growth, newpop= 140000;
+       int year;
+       Console c = new Console();
+       pop=140000;
+       c.print(Format.justify('r', "year", 6));
+       c.print(Format.justify('r', "population",20));
+       c.print(Format.justify('r', "growth", 20));
+       c.print(Format.justify('r', "new", 20));
+       for (year=2015;year<=2115;year+=10)
+       {
+           c.print(Format.justify('r', year, 6));
+           c.print(Format.justify('r', pop, 20,0));
+           growth = pop * 0.03;
+           newpop+=growth;
+           c.print(Format.justify('r', growth, 20,0));
+           c.print(Format.justify('r', newpop, 20,0));
+           pop=newpop;
+           
+                   
+       }
+    }
+    
+}
