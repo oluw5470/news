28cce2527ef31d49dbb491545c06cf934f666e78
oluw5470 <oluw5470@ISS147D8-11.scdsb.pri>
2015-09-18 10:22:09 -0400

    nreal commit

--- src/sample.java
+++ src/sample.java
@@ -10,7 +10,7 @@ public class sample {
        c.print(Format.justify('r', "year", 6));
        c.print(Format.justify('r', "population",20));
        c.print(Format.justify('r', "growth", 20));
-       c.print(Format.justify('r', "new", 20));
+       c.println(Format.justify('r', "new population", 20)+"\n");
        for (year=2015;year<=2115;year+=10)
        {
            c.print(Format.justify('r', year, 6));
@@ -18,7 +18,7 @@ public class sample {
            growth = pop * 0.03;
            newpop+=growth;
            c.print(Format.justify('r', growth, 20,0));
-           c.print(Format.justify('r', newpop, 20,0));
+           c.println(Format.justify('r', newpop, 20,0));
            pop=newpop;
            
                    
