a108d88619c7c1a421834f8ceb312b497427b052
oluw5470 <oluw5470@ISS147D8-11.scdsb.pri>
2015-09-21 10:30:10 -0400

    nreal commit

--- src/powers.java
+++ src/powers.java
@@ -5,12 +5,14 @@ public class powers {
     public static void main(String[] args) {
         Console c = new Console();
        int num,anum,rnum;
-       
-       anum=2;
+       c.print("plz enter the first number ");
+      
        rnum= c.readInt();
-       for(int x=0; x<=rnum; x++)
+       for(int x=1; x<=rnum; x++)
        {
            num=(int)Math.pow(2,x);
+           c.println(x+"\t"+num);
+                   
        }
        
       
