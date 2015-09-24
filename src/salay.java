e9628e8ec9a3ffff396940f4f755c1b962fe5828
oluw5470 <oluw5470@ISS147D8-11.scdsb.pri>
2015-09-24 11:33:29 -0400

    nreal commit

--- src/salay.java
+++ src/salay.java
@@ -6,17 +6,31 @@ public class salay {
     public static void main(String[] args) {
         Console c= new Console();
         double pay, raise,incp,npay,opay;
-        double year=0;
+        int year;
         pay=40000;
-        
-        for(int x=0;x<=10;x++)
+        c.print(Format.justify('r', "year", 6));
+        c.print(Format.justify('r', "Old Salary",20));
+        c.print(Format.justify('r', "Raise", 20));
+        c.println(Format.justify('r', "New Salary", 20)+"\n");
+        for(year=0;year<=10;year++)
         {
-        year++;
+        
+        c.print(Format.justify('r', year, 6));
         incp=year * .03;
         raise= incp*pay;
         npay= raise +pay;
-        opay= npay-1200;
-        c.print(opay);
+        opay= npay-2400;
+           if(opay<40000)
+           {
+           c.print(Format.justify('r', 0, 20,0));
+           }
+           else
+               c.print(Format.justify('r', opay, 20,0));
+           
+           c.print(Format.justify('r', raise, 20,0));
+           c.println(Format.justify('r', npay, 20,0));
+           
+       
         }
         
     }
